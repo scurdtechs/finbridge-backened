fef919a0f4220082cbcd04f254ed8a009c59b32c
scurd142-glitch <scurd142@gmail.com>
2026-04-12 13:49:04 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -50,6 +50,7 @@ const defaultAllowedOrigins = [
   "http://localhost:3000",
   "https://finbridge-fronted-6ru9.vercel.app",
   "https://finbridge-fronted-qi4n.vercel.app",
+  "https://finbridge-fronted-hzte.vercel.app",
 ];
 
 const allowedOrigins = process.env.CLIENT_ORIGINS
